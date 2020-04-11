bf7ac54e8a25157b0caa150afe4e0f0298f41ab2
Zviad Metreveli <zviad@dropbox.com>
2020-04-11 16:10:13 -0400

    add simple rpc request metrics

--- handlers/grpcstats/server.go
+++ handlers/grpcstats/server.go
@@ -2,8 +2,11 @@ package grpcstats
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/zviadm/stats-go/metrics"
 	"google.golang.org/grpc/stats"
+	"google.golang.org/grpc/status"
 )
 
 type serverHandler struct{}
@@ -13,13 +16,27 @@ func NewServer() stats.Handler {
 	return &serverHandler{}
 }
 
+type ctxRpcTagInfo struct{}
+
+var rpcTagInfoKey = ctxRpcTagInfo{}
+
 func (h *serverHandler) TagRPC(
 	ctx context.Context, info *stats.RPCTagInfo) context.Context {
-	return ctx
+	return context.WithValue(ctx, rpcTagInfoKey, info)
 }
 
 func (h *serverHandler) HandleRPC(ctx context.Context, s stats.RPCStats) {
-
+	if s.IsClient() {
+		return
+	}
+	switch s := s.(type) {
+	case *stats.End:
+		info := ctx.Value(rpcTagInfoKey).(*stats.RPCTagInfo)
+		serverRequestsCounter.V(metrics.KV{
+			"method": info.FullMethodName,
+			"code":   strconv.Itoa(int(status.Convert(s.Error).Code())),
+		}).Count(1)
+	}
 }
 
 func (h *serverHandler) TagConn(ctx context.Context, info *stats.ConnTagInfo) context.Context {
@@ -32,10 +49,10 @@ func (h *serverHandler) HandleConn(ctx context.Context, s stats.ConnStats) {
 	}
 	switch s := s.(type) {
 	case *stats.ConnBegin:
-		connectionsServerG.Add(1)
-		connectsServerG.Count(1)
+		serverConnectionsG.Add(1)
+		serverConnectsC.Count(1)
 	case *stats.ConnEnd:
-		connectionsServerG.Add(-1)
+		serverConnectionsG.Add(-1)
 		_ = s
 	}
 }
--- handlers/grpcstats/stats.go
+++ handlers/grpcstats/stats.go
@@ -3,8 +3,13 @@ package grpcstats
 import "github.com/zviadm/stats-go/metrics"
 
 var (
-	connectionsServerG = metrics.DefineGauge("grpc/server/connetions").V()
-	connectsServerG    = metrics.DefineCounter("grpc/server/connects").V()
+	serverConnectionsG = metrics.DefineGauge("grpc/server/connetions").V()
+	serverConnectsC    = metrics.DefineCounter("grpc/server/connects").V()
+
+	serverRequestsCounter = metrics.DefineCounter(
+		"grpc/server/requests",
+		metrics.WithTags("method", "code"),
+	)
 
 	// connectionsClientG = metrics.DefineGauge("grpc/client/connetions").V()
 	// connectsClientG    = metrics.DefineCounter("grpc/client/connects").V()
