12a3fe0ee52b85cb93027a2b305ab5fdb6570633
Zviad Metreveli <zviad@dropbox.com>
2020-04-11 09:47:00 -0400

    beginnings of grpc stats handler

---
+++ handlers/grpcstats/go.mod
@@ -0,0 +1,10 @@
+module github.com/zviadm/stats-go/handlers/grpc
+
+go 1.14
+
+require (
+	github.com/zviadm/stats-go/metrics v0.0.0
+	google.golang.org/grpc v1.28.1
+)
+
+replace github.com/zviadm/stats-go/metrics => ../../metrics
---
+++ handlers/grpcstats/stats.go
@@ -0,0 +1,11 @@
+package grpcstats
+
+import "github.com/zviadm/stats-go/metrics"
+
+var (
+	connectionsServerG = metrics.DefineGauge("grpc/server/connetions").V()
+	connectsServerG    = metrics.DefineCounter("grpc/server/connects").V()
+
+	connectionsClientG = metrics.DefineGauge("grpc/client/connetions").V()
+	connectsClientG    = metrics.DefineCounter("grpc/client/connects").V()
+)
